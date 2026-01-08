bf271b60925728641136da34cd7457392c323f96
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-01-09 01:55:27 +0530

    Create Contact, Portfolio, Resume, and Services components

---
+++ my-portfolio/src/app/pages/portfolio/portfolio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../component/navbar/navbar.component";
+
+@Component({
+  selector: 'app-portfolio',
+  imports: [NavbarComponent],
+  templateUrl: './portfolio.component.html',
+  styleUrl: './portfolio.component.css'
+})
+export class PortfolioComponent {
+
+}
---
+++ my-portfolio/src/app/pages/resume/resume.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../component/navbar/navbar.component";
+
+@Component({
+  selector: 'app-resume',
+  imports: [NavbarComponent],
+  templateUrl: './resume.component.html',
+  styleUrl: './resume.component.css'
+})
+export class ResumeComponent {
+
+}
