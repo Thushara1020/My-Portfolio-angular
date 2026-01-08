4ea2d36e26e7045dc172d9a7ea352b5c940e6d10
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-01-08 22:19:25 +0530

    implement main navbar with routing links

---
+++ my_portfolio_angular/src/app/component/navmenu/navmenu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-navmenu',
+  imports: [RouterLink],
+  templateUrl: './navmenu.component.html',
+  styleUrl: './navmenu.component.css'
+})
+export class NavmenuComponent {
+
+}
