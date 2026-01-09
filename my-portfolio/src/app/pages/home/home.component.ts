9007711857535ce2f2d1b8ae7aeef895244b0ac1
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-01-09 05:36:07 +0530

    Add Logo img

--- my-portfolio/src/app/pages/home/home.component.ts
+++ my-portfolio/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { NavbarComponent } from "../../component/navbar/navbar.component";
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,29 @@ import { NavbarComponent } from "../../component/navbar/navbar.component";
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit, OnDestroy {
 
+
+  @ViewChild('typedElement') typedElement!: ElementRef;
+  typed!: Typed;
+
+  ngAfterViewInit() {
+    const options = {
+      strings: ['UI/UX Designer', 'Web Developer', 'Software Engineer'],
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+      loop: true
+    };
+
+    if (this.typedElement) {
+      this.typed = new Typed(this.typedElement.nativeElement, options);
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.typed) {
+      this.typed.destroy();
+    }
+  }
 }
