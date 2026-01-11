47badc705f74b71a637c9220ddd7203638c4cdbe
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-01-11 22:29:03 +0530

    Integrate footer component into about page layout

--- my-portfolio/src/app/pages/about/about.component.ts
+++ my-portfolio/src/app/pages/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../component/navbar/navbar.component";
+import { FooterComponent } from "../../component/footer/footer.component";
 
 @Component({
   selector: 'app-about',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
