84702ab70ec795e72c89b5c69de0e8bdc5b6a44b
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-01-11 22:30:45 +0530

    Integrate footer component into resume page layout

--- my-portfolio/src/app/pages/resume/resume.component.ts
+++ my-portfolio/src/app/pages/resume/resume.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../component/navbar/navbar.component";
+import { FooterComponent } from "../../component/footer/footer.component";
 
 @Component({
   selector: 'app-resume',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
