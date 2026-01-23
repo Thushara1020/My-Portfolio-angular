94b0e511f1fa56e8990f57ff5b50da620949773e
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-01-23 13:34:02 +0530

    migrate to signal-based inputs in component

--- my-portfolio/src/app/pages/services/services.component.ts
+++ my-portfolio/src/app/pages/services/services.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { NavbarComponent } from "../../component/navbar/navbar.component";
 import { FooterComponent } from "../../component/footer/footer.component";
